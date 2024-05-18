2e778ffeca840d88f32f9e00e919cbd35d9f8604
am00473656 <anuj.mundhra93@gmail.com>
2024-05-18 20:04:37 +0530

    revision fulfilled

--- assignments/palak/decorator_revision.py
+++ assignments/palak/decorator_revision.py
@@ -1,1 +1,28 @@
 ''' rivision of decorators'''
+
+"""
+Decorators: Add additional functionality on top of main functionality
+"""
+
+
+def outer(func):
+    def inner(*args, **kwargs):
+        print("Additonal Functionality")
+        return func(*args, **kwargs)
+    return inner
+
+
+@outer  # adder = outer(adder) = inner
+def adder(a, b):
+    print(a+b)
+    return a+b
+
+# @outer # sub = outer(sub)
+# def sub(a, b, c):
+#     print(a-b)
+
+
+output = adder(5, 6) # inner(5, 6)
+print(output)
+
+# sub(3, 2)
